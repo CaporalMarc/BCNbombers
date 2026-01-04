26340242f3bdd11955a06b37e308cd7cf19f3d6b
CaporalMarc <marcrubieswestudios@gmail.com>
2026-01-04 05:14:48 +0100

    Create noticias.js

---
+++ noticias.js
@@ -0,0 +1,18 @@
+fetch('noticias.json')
+  .then(res => res.json())
+  .then(data => {
+    const contenedor = document.getElementById('noticias');
+
+    data.forEach(noticia => {
+      const card = document.createElement('div');
+      card.className = 'noticia-card';
+
+      card.innerHTML = `
+        <h3>${noticia.titulo}</h3>
+        <small>${noticia.fecha}</small>
+        <p>${noticia.texto}</p>
+      `;
+
+      contenedor.appendChild(card);
+    });
+  });
